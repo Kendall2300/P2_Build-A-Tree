4d96a3631ad2506bf406f24d49026ec65826c2cb
Gambo2907 <67343582+Gambo2907@users.noreply.github.com>
2020-12-07 18:00:08 -0600

    Server 2
    Ya puede enviar y recibir mensajes, además cierra el servidor

--- src/Server.java
+++ src/Server.java
@@ -12,26 +12,30 @@ public class Server {
         ServerSocket serverSocket = new ServerSocket(port);
         while(true){
             Socket socket = serverSocket.accept();
-            try{
-                Scanner scanner = new Scanner(System.in);
-                InputStream request = socket.getInputStream();
-                DataInputStream dis = new DataInputStream(request);
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                String message = new String(dis.readAllBytes());
-                System.out.println("Mensaje recibido :" + message);
+            Scanner scanner = new Scanner(System.in);
+            InputStream dis = socket.getInputStream();
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            try {
                 String tosend = scanner.nextLine();
                 dos.writeUTF(tosend);
-                System.out.println("Mensaje enviado");
+                System.out.println("Mensaje enviado ");
+                String message = new String(dis.readAllBytes());
+                System.out.println("Mensaje recibido: " + message);
+                if(message.equals("Exit")) {
+                    System.out.println("Cerrando el servidor");
+                    socket.close();
+                    dis.close();
+                    dos.close();
+                    scanner.close();
+                    System.out.println("Se cerró el servidor");
+                    break;
+                }
 
-
-
-            } catch(Exception e){
+            }catch (Exception e){
                 e.printStackTrace();
             }
 
 
         }
-
-
     }
 }
