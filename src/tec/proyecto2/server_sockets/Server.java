60bd8ddd333da4aa9a7fc3903af92634223af142
Gambo2907 <67343582+Gambo2907@users.noreply.github.com>
2020-12-21 17:39:38 -0600

    5

--- src/tec/proyecto2/server_sockets/Server.java
+++ src/tec/proyecto2/server_sockets/Server.java
@@ -1,11 +1,11 @@
 package tec.proyecto2.server_sockets;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Random;
+
 
 /***
  * Clase Server la cual está encargada de escuchar por medio de sockets cuando un jugador obtiene un token,
@@ -17,53 +17,25 @@ public class Server {
          Para la comunicación por medio de sockets en el server ya está establecido un puerto en el cuál será
          escuchado el cliente correspondiente
          */
-        String tosend = "Bienvenido";
+        String tosend ="Bienvenido";
         int port = 6000;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println(tosend);
         /***
-         * EL while(true) proporciona el bucle para que así no se cierre el servidor y se pierda contacto
+          EL while(true) proporciona el bucle para que así no se cierre el servidor y se pierda contacto
          */
         while(true){
             Socket socket = serverSocket.accept();
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            InputStream dis = socket.getInputStream();
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
             try {
                 dos.writeUTF(tosend);
                 String message = new String(dis.readAllBytes());
-                System.out.println("Mensaje recibido: " + message);
-                /***
-                 * En esta función if se recibirá un mensaje del cliente a la hora de tocar el botón "play", de
-                 * esta manera el servidor mandará por medio de socket el challenge correspondiente al juego de
-                 * manera aleatoria con el Random
-                 */
-                if(message.equals("Play")) {
-                    Random rand = new Random();
-                    int upperbound = 4;
-                    int int_random = rand.nextInt(upperbound);
+                System.out.println(message);
+                
 
-                    if(int_random==0){
-                        tosend = "Build BST";
-                        dos.writeUTF(tosend);
-                        System.out.println("Este es tu challenge: " + tosend);
-                    }
-                    else if(int_random==1){
-                        tosend = "Build B Tree";
-                        dos.writeUTF(tosend);
-                        System.out.println("Este es tu challenge: " + tosend);
-
-                    } else if (int_random==2) {
-                        tosend = "Build AVL";
-                        dos.writeUTF(tosend);
-                        System.out.println("Este es tu challenge: " + tosend);
-                    } else if(int_random == 3){
-                        tosend = "Build Splay";
-                        dos.writeUTF(tosend);
-                        System.out.println("Este es tu challenge: " + tosend);
-                    }
-                }
                 /***
-                 * Esta función if cierra el servidor cuando se cierre el cliente correspondiente
+                 * Este condicional if cierra el servidor cuando se cierre el cliente correspondiente
                  */
                 if(message.equals("Exit")) {
                     System.out.println("Cerrando el servidor");
